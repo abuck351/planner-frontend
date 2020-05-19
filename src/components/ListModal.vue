b9e8cf224eb725df3c26843a9a5cd9a0e7430126
Aaron Buckles <abuck351@gmail.com>
2020-05-19 16:50:42 -0700

    Adds time to list view

--- src/components/ListModal.vue
+++ src/components/ListModal.vue
@@ -21,7 +21,7 @@
           <ul>
             <li>
               <i class="fas fa-clock"></i>
-              {{ course.days }}
+              {{ daysAndTime(course.days, course.start_time, course.end_time) }}
             </li>
             <li>
               <i class="fas fa-chalkboard-teacher"></i>
@@ -44,14 +44,30 @@
 
 <script>
 import { mapGetters } from "vuex";
+import { formatRange } from "@fullcalendar/core";
 
 export default {
   name: "ListModal",
   computed: {
     ...mapGetters(["currentPlan"]),
-    courses() {
+    courses: function() {
       if (this.currentPlan) return this.currentPlan.courses;
       return null;
+    }
+  },
+  methods: {
+    daysAndTime(days, start_time, end_time) {
+      // Date doesn't matter b/c I just care about the time
+      const start = new Date(`January 1, 2000 ${start_time}`);
+      const end = new Date(`January 1, 2000 ${end_time}`);
+
+      const timeRange = formatRange(start, end, {
+        hour12: true,
+        hour: "numeric",
+        minute: "2-digit",
+        separator: " to "
+      });
+      return `${days} from ${timeRange}`;
     }
   }
 };
